f52e0642e21cf12a3a42a0b12211baeaaceb06cc
yongleyuan <yongle.jack.yuan@gmail.com>
2024-06-13 20:20:52 +0800

    Modify keymaps, add pyright

--- init.lua
+++ init.lua
@@ -194,9 +194,10 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- Navigate/manage buffers
-vim.keymap.set('n', '<S-j>', ':bnext<CR>', { desc = 'Move to the next buffer' })
-vim.keymap.set('n', '<S-k>', ':bprevious<CR>', { desc = 'Move to the previous buffer' })
-vim.keymap.set('n', '<S-d>', ':bdelete<CR>', { desc = 'Delete current buffer' })
+vim.keymap.set('n', 'J', ':bnext<CR>', { desc = 'Move to the next buffer' })
+vim.keymap.set('n', 'K', ':bprevious<CR>', { desc = 'Move to the previous buffer' })
+vim.keymap.set('n', 'D', ':bdelete<CR>', { desc = 'Delete current buffer' })
+vim.keymap.set('n', 'DF', ':bdelete!<CR>', { desc = 'Force delete current buffer' })
 
 -- Press jk fast to exit insert mode
 vim.keymap.set('i', 'jk', '<ESC>')
@@ -578,7 +579,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -729,9 +730,9 @@ require('lazy').setup({
         -- No, but seriously. Please read `:help ins-completion`, it is really good!
         mapping = cmp.mapping.preset.insert {
           -- Select the [n]ext item
-          ['<C-n>'] = cmp.mapping.select_next_item(),
+          -- ['<C-n>'] = cmp.mapping.select_next_item(),
           -- Select the [p]revious item
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          -- ['<C-p>'] = cmp.mapping.select_prev_item(),
 
           -- Scroll the documentation window [b]ack / [f]orward
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -740,13 +741,13 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          -- ['<C-y>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
-          --['<CR>'] = cmp.mapping.confirm { select = true },
-          --['<Tab>'] = cmp.mapping.select_next_item(),
-          --['<S-Tab>'] = cmp.mapping.select_prev_item(),
+          ['<CR>'] = cmp.mapping.confirm { select = true },
+          ['<Tab>'] = cmp.mapping.select_next_item(),
+          ['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
