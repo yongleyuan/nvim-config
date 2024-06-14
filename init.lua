7d93eb5d8d3f1079706f08b8ee5b2564bf24c715
yongleyuan <yongle.jack.yuan@gmail.com>
2024-06-14 14:04:32 +0800

    Install aerial

--- init.lua
+++ init.lua
@@ -882,6 +882,15 @@ require('lazy').setup({
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
+  {
+    'stevearc/aerial.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = {
+       "nvim-treesitter/nvim-treesitter",
+       "nvim-tree/nvim-web-devicons"
+    },
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
