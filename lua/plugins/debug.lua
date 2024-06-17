41d8b2b083023187634e656449d09543a72fba08
Yongle Yuan <yongle.jack.yuan@gmail.com>
2024-06-17 21:26:12 +0800

    Add python dap

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -9,6 +9,7 @@
 return {
   -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
+  'mfussenegger/nvim-dap-python',
   -- NOTE: And you can specify dependencies as well
   dependencies = {
     -- Creates a beautiful debugger UI
@@ -92,5 +93,7 @@ return {
         detached = vim.fn.has 'win32' == 0,
       },
     }
+
+    require('dap-python').setup 'python'
   end,
 }
