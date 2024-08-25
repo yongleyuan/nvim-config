7fe48450477f1ecf28cb510328a6f9126305efde
Yongle Yuan <yongle.jack.yuan@gmail.com>
2024-08-25 14:56:39 -0400

    New lsp config, goto lsp not working

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -22,53 +22,26 @@ return { -- LSP Configuration & Plugins
     },
   },
   config = function()
-    -- LSP servers and clients are able to communicate to each other what features they support.
-    --  By default, Neovim doesn't support everything that is in the LSP specification.
-    --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-    --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
-    -- Enable the following language servers
-    --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-    --
-    --  Add any additional override configuration in the following tables. Available keys are:
-    --  - cmd (table): Override the default command used to start the server
-    --  - filetypes (table): Override the default list of associated filetypes for the server
-    --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-    --  - settings (table): Override the default settings passed when initializing the server.
-    --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
-      -- clangd = {},
-      -- gopls = {},
       pyright = {
-        settings = {
-          python = {
-            analysis = {
-              diagnosticSeverityOverrides = {
-                reportUnusedClass = 'warning',
-                reportUnusedFunction = 'warning', -- TODO: This does not work!
-                reportUnusedImport = 'warning',
-                reportUnusedVariable = 'warning',
-              },
-            },
-          },
-        },
+        -- settings = {
+        --   python = {
+        --     analysis = {
+        --       diagnosticSeverityOverrides = {
+        --         reportUnusedClass = 'warning',
+        --         reportUnusedFunction = 'warning', -- TODO: This does not work!
+        --         reportUnusedImport = 'warning',
+        --         reportUnusedVariable = 'warning',
+        --       },
+        --     },
+        --   },
+        -- },
       },
-      -- rust_analyzer = {},
-      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`tsserver`) will work just fine
-      -- tsserver = {},
-      --
-
+      -- ruff = {},
       lua_ls = {
-        -- cmd = {...},
-        -- filetypes = { ...},
-        -- capabilities = {},
         settings = {
           Lua = {
             completion = {
@@ -93,14 +66,9 @@ return { -- LSP Configuration & Plugins
     -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
-      'black',
-      'debugpy',
-      'jsonlint',
       'lua_ls',
       'markdownlint',
-      'pylint',
-      'pyright',
-      'stylua',
+      'prettier',
     })
 
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
@@ -113,7 +81,7 @@ return { -- LSP Configuration & Plugins
           -- by the server configuration above. Useful when disabling
           -- certain features of an LSP (for example, turning off formatting for tsserver)
           server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          -- vim.notify(server.settings.python.analysis.diagnosticSeverityOverrides.reportUnusedClass)
+          require('lspconfig')[server_name].setup(server)
         end,
       },
     }
