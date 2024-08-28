3c920a62fae192ed1319c3945486cd5519304201
Yongle Yuan <yongle.jack.yuan@gmail.com>
2024-08-28 18:40:20 -0400

    Set scrolloff to 5

--- init.lua
+++ init.lua
@@ -72,7 +72,7 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 5
 
 -- Use terminal gui colors
 vim.opt.termguicolors = true
