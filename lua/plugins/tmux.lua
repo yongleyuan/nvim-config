590443d2eb90c6258a11076f36369a69e19cbb4c
Yongle Yuan <yongle.jack.yuan@gmail.com>
2024-10-18 21:11:06 -0400

    Add harpoon telescope, configure tmux yank

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -2,6 +2,9 @@ return {
   'aserowy/tmux.nvim',
   config = function()
     return require('tmux').setup {
+      copy_sync = {
+        redirect_to_clipboard = true,
+      },
       navigation = {
         cycle_navigation = false,
       },
