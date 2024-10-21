157c41c5556aa0badc117b18718a1c2b12edf33d
Yongle Yuan <yongle.jack.yuan@gmail.com>
2024-10-21 15:43:17 -0400

    Get rid of execution message for auto save

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -4,14 +4,6 @@ return {
   event = { 'InsertLeave', 'TextChanged' }, -- optional for lazy loading on trigger events
   opts = {
     enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
-    execution_message = {
-      enabled = true,
-      message = function() -- message to print on save
-        return ('auto-save: saved at ' .. vim.fn.strftime '%H:%M:%S')
-      end,
-      dim = 0.18, -- dim the color of `message`
-      cleaning_interval = 1250, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
-    },
     trigger_events = { -- See :h events
       immediate_save = { 'BufLeave', 'FocusLost', 'InsertLeave', 'TextChanged' }, -- vim events that trigger an immediate save
       -- defer_save = { 'InsertLeave', 'TextChanged' }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
