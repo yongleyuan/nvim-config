fff46b65661609a2c8aa81f9443ce771170ce3de
Yongle Yuan <yongle.jack.yuan@gmail.com>
2024-12-01 23:37:25 -0500

    Disableq: command history

--- init.lua
+++ init.lua
@@ -88,6 +88,8 @@ vim.opt.hlsearch = true
 
 vim.opt.conceallevel = 1
 
+vim.keymap.set('n', 'q:', '<nop>', { silent = true })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
