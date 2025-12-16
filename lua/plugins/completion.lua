52996b877dc0be6c0dbcfe194e52b9be69a5a632
yongleyuan <yongle.jack.yuan@gmail.com>
2025-12-16 15:57:47 +0800

    nvim: disable blink signature

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -18,11 +18,10 @@ return {
         end,
         'fallback',
       },
-
     },
     cmdline = { completion = { menu = { auto_show = true } } },
     appearance = { use_nvim_cmp_as_default = true, nerd_font_variant = 'mono' },
     sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
-    signature = { enabled = true },
+    signature = { enabled = false },
   },
 }
